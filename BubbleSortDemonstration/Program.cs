5e7e0b808703dd5dbe19d1d0f66f2a28a9982b37
occulus1975 <occulus1975@gmail.com>
2020-06-04 19:48:49 -0400

    HHHmmmm. Something isn't right at all. Sorts take FAR too long.

--- BubbleSortDemonstration/Program.cs
+++ BubbleSortDemonstration/Program.cs
@@ -12,16 +12,16 @@ namespace BubbleSortDemonstration
 
             int[] arraySizes =
             {
-                10, //100, 1000, 10000, 25000, 50000, 100000, 250000, 500000, 750000, 1000000
+                /*10, 100, 1000, 10000, 25000, 50000, 100000, 250000, 500000, 750000, */ 1000000,
             };
 
             foreach (int size in arraySizes)
             {
                 rand = new Random();
                 ArrayToSort newArray = new ArrayToSort(size, rand);
+                Console.WriteLine("Collection created.");
                 //Console.WriteLine(newArray.ToString());
             }
-            Console.WriteLine("Collection created.");
             Environment.Exit(0);
         }
 
